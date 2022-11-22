74b05dde631ac7aa7fc9f7fff1ca0028dd90e33a
seitenca <alexdltg@protonmail.com>
2022-11-22 21:51:24 +0100

    first commit

---
+++ src/config.ts
@@ -0,0 +1,146 @@
+import type { MarkdownInstance } from 'astro'
+
+export interface Frontmatter {
+  draft?: boolean
+  title: string
+  description?: string
+  author?: string
+  publishDate: string
+  coverSVG?: string
+  coverImage?: string
+  socialImage?: string
+  categories?: string[]
+  tags?: string[]
+  file?: string
+  url?: string
+  minutesRead?: string
+  extra?: string[]
+  section?: string[]
+}
+
+export interface TagType {
+  tag: string
+  count: number
+  pages: MarkdownInstance<Frontmatter>[]
+}
+
+export const SiteMetadata = {
+  title: 'Sommcasa',
+  description: '',
+  author: {
+    name: 'Chris Tham',
+    twitter: '@chris1tham',
+    url: 'https://christham.net',
+    email: 'chris@christham.net',
+    summary: 'Outrageous actualiser.',
+  },
+  org: {
+    name: 'Hello Tham',
+    twitter: '@hellothamcom',
+    url: 'https://hellotham.com',
+    email: 'info@hellotham.com',
+    summary:
+      'Hello Tham is a boutique management consulting firm. We specialise in Business and IT strategies, operating models, strategic roadmaps, enterprise architecture, analytics and business process design.',
+  },
+  location: 'Rivendell, Middle Earth',
+  latlng: [-33.86785, 151.20732] as [number, number],
+  repository: 'https://github.com/hellotham/hello-astro',
+  social: [
+    {
+      name: 'Facebook',
+      link: 'https://www.facebook.com/sommcasa/',
+      icon: 'facebook',
+    },
+    {
+      name: 'Instagram',
+      link: 'https://www.instagram.com/sommcasa/',
+      icon: 'instagram',
+    },
+  ],
+  buildTime: new Date().toString(),
+}
+
+export const Logo = '../svg/sommcasa/logo.svg'
+export const LogoImage = '../images/sommcasa/sommcasa_white.png'
+export const FeaturedSVG = '../svg/undraw/undraw_design_inspiration.svg'
+export const DefaultSVG = '../svg/undraw/undraw_my_feed.svg'
+export const DefaultImage = '../images/undraw/undraw_my_feed.png'
+
+export const NavigationLinks = [
+  { name: 'Tiendas', href: '' },
+  { name: 'Cocinas', href: '' },
+  { name: 'Zona Día', href: '' },
+  { name: 'Zona Noche', href: '' },
+  { name: 'Blog', href: '' },
+  { name: 'Sobre Nosotros', href: '' },
+]
+
+export const CategoryDetail = [
+  {
+    category: 'instructions',
+    coverSVG: '../svg/undraw/undraw_instruction_manual.svg',
+    socialImage: '../images/undraw/undraw_instruction_manual.png',
+    description: 'Guidelines on using this starter.',
+  },
+  {
+    category: 'information',
+    coverSVG: '../svg/undraw/undraw_instant_information.svg',
+    socialImage: '../images/undraw/undraw_instant_information.png',
+    description: 'Information articles.',
+  },
+]
+
+export function categoryDetail(category: string | undefined) {
+  const details = CategoryDetail.filter(cat => cat.category == category)
+
+  if (details.length == 1) {
+    return details[0]
+  }
+  return {
+    category: 'General',
+    coverSVG: '../svg/undraw/undraw_instant_information.svg',
+    socialImage: '../images/undraw/undraw_instant_information.png',
+    description: 'Category ' + category,
+  }
+}
+export const AuthorDetail = [
+  {
+    name: 'Alexia Dltg',
+    description: 'Hello World',
+    contact: 'alexia@alexia.alexia',
+    image: '../images/authors/Chris Tham.jpg',
+  },
+]
+
+export const DefaultAuthor = {
+  name: 'Hello Astro',
+  image: '../images/authors/default.png',
+  contact: 'info@hellotham.com',
+  description: 'Astronaut',
+}
+
+export function authorDetail(author: string | undefined) {
+  const details = AuthorDetail.filter(person => person.name == author)
+
+  if (details.length == 1) {
+    return details[0]
+  }
+  return DefaultAuthor
+}
+
+export const PAGE_SIZE = 6
+
+export const GITHUB_EDIT_URL = `https://github.com/hellotham/hello-astro`
+
+export const COMMUNITY_INVITE_URL = `https://astro.build/chat`
+
+export type Sidebar = Record<string, { text: string; link: string }[]>
+
+export const SIDEBAR: Sidebar = {
+  'Section Header': [
+    { text: 'Introduction', link: 'doc/introduction' },
+    { text: 'Page 2', link: 'doc/page-2' },
+    { text: 'Page 3', link: 'doc/page-3' },
+  ],
+  'Another Section': [{ text: 'Page 4', link: 'doc/page-4' }],
+}
